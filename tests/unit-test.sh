dd60d202d217037e5f8640526eea17efc1d5d614
pli01 <pli01@github.com>
2018-02-20 16:29:17 +0000

    prefix compose test namespace

--- tests/unit-test.sh
+++ tests/unit-test.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
+set -x
 
 image_name=${1:? $(basename $0) IMAGE_NAME VERSION needed}
 version=${2:-latest}
+namespace=jenkins
 
-echo "Check docker-compose config"
-docker-compose config
+echo "Check docker-compose -p ${namespace} config"
+docker-compose -p ${namespace} config
 
 echo "Check Jenkins version"
-docker-compose run --rm jenkins --version
+docker-compose -p ${namespace} run --rm jenkins --version
 
 echo "Check docker version"
 docker run -it --rm $image_name /bin/bash -c 'docker version || true'
-docker run -it --rm $image_name /bin/bash -c 'docker-compose version || true'
+docker run -it --rm $image_name /bin/bash -c 'docker-compose -p ${namespace} version || true'
 
-#docker-compose run --name "test" --rm --entrypoint '/bin/bash' jenkins -x /test/unit/version.sh
-#docker-compose run --name "test" --rm --entrypoint '/bin/bash' jenkins -c /usr/local/bin/docker-compose
+#docker-compose -p ${namespace} run --name "test" --rm --entrypoint '/bin/bash' jenkins -x /test/unit/version.sh
+#docker-compose -p ${namespace} run --name "test" --rm --entrypoint '/bin/bash' jenkins -c /usr/local/bin/docker-compose
 exit 0
